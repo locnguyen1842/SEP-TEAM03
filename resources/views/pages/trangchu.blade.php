b9c6aa355ecf5864c4f8ca6923f5863b553c9c1c
TueKhuong <lamtuekhuong@gmail.com>
2018-05-27 22:38:38 +0700

    chinh sua slider

--- resources/views/pages/trangchu.blade.php
+++ resources/views/pages/trangchu.blade.php
@@ -1,163 +1,160 @@
 
 @extends('master')
 @section('content')
-<div class="fullwidthbanner-container">
-					<div class="fullwidthbanner">
-						<div class="bannercontainer" >
-					    <div class="banner" >
-								<ul>
-									@foreach($slide as $sl)
+<link rel="stylesheet" title="style" href="source/assets/dest/css/slider.css">
 
-									<!-- THE FIRST SLIDE -->
-									<li data-transition="boxfade" data-slotamount="20" class="active-revslide" style="width: 100%; height: 100%; overflow: hidden; z-index: 18; visibility: hidden; opacity: 0;">
-						            <div class="slotholder" style="width:100%;height:100%;" data-duration="undefined" data-zoomstart="undefined" data-zoomend="undefined" data-rotationstart="undefined" data-rotationend="undefined" data-ease="undefined" data-bgpositionend="undefined" data-bgposition="undefined" data-kenburns="undefined" data-easeme="undefined" data-bgfit="undefined" data-bgfitend="undefined" data-owidth="undefined" data-oheight="undefined">
-													<div class="tp-bgimg defaultimg" data-lazyload="undefined" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat" data-lazydone="undefined" src="source/image/slide/{{ $sl->image }}" data-src="source/image/slide/{{ $sl->image }}" style="background-color: rgba(0, 0, 0, 0); background-repeat: no-repeat; background-image: url('source/image/slide/{{ $sl->image }}'); background-size: cover; background-position: center center; width: 100%; height: 100%; opacity: 1; visibility: inherit;">
-													</div>
-												</div>
+				<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    @for($i = 1 ; $i< count($slide) +1 ;$i++)
+    <li data-target="#myCarousel" data-slide-to="{{ $i }}"></li>
+    
+   @endfor
+  </ol>
 
-						        </li>
-						        @endforeach
-								{{-- <li data-transition="boxfade" data-slotamount="20" class="active-revslide" style="width: 100%; height: 100%; overflow: hidden; z-index: 18; visibility: hidden; opacity: 0;">
-						          <div class="slotholder" style="width:100%;height:100%;" data-duration="undefined" data-zoomstart="undefined" data-zoomend="undefined" data-rotationstart="undefined" data-rotationend="undefined" data-ease="undefined" data-bgpositionend="undefined" data-bgposition="undefined" data-kenburns="undefined" data-easeme="undefined" data-bgfit="undefined" data-bgfitend="undefined" data-owidth="undefined" data-oheight="undefined">
-												<div class="tp-bgimg defaultimg" data-lazyload="undefined" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat" data-lazydone="undefined" src="source/assets/dest/images/thumbs/1.jpg" data-src="source/assets/dest/images/thumbs/1.jpg" style="background-color: rgba(0, 0, 0, 0); background-repeat: no-repeat; background-image: url('assets/dest/images/thumbs/1.jpg'); background-size: cover; background-position: center center; width: 100%; height: 100%; opacity: 1; visibility: inherit;">
-											</div>
-											</div>
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+  	<div class="item active">
+      <img src="source/image/slide/{{ $slideA->image }}" alt="Chania"  width="100%" height="450px">
+    </div>
+  @foreach($slide as $s)
+ 
+    <div class="item">
+      <img src="source/image/slide/{{ $s->image }}" alt="Chania"  width="100%" height="450px">
+    </div>
+@endforeach
+  </div>
 
-								<li data-transition="boxfade" data-slotamount="20" class="active-revslide" style="width: 100%; height: 100%; overflow: hidden; z-index: 18; visibility: hidden; opacity: 0;">
-						            <div class="slotholder" style="width:100%;height:100%;" data-duration="undefined" data-zoomstart="undefined" data-zoomend="undefined" data-rotationstart="undefined" data-rotationend="undefined" data-ease="undefined" data-bgpositionend="undefined" data-bgposition="undefined" data-kenburns="undefined" data-easeme="undefined" data-bgfit="undefined" data-bgfitend="undefined" data-owidth="undefined" data-oheight="undefined">
-													<div class="tp-bgimg defaultimg" data-lazyload="undefined" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat" data-lazydone="undefined" src="source/assets/dest/images/thumbs/1.jpg" data-src="source/assets/dest/images/thumbs/1.jpg" style="background-color: rgba(0, 0, 0, 0); background-repeat: no-repeat; background-image: url('assets/dest/images/thumbs/1.jpg'); background-size: cover; background-position: center center; width: 100%; height: 100%; opacity: 1; visibility: inherit;">
-												</div>
-											</div>
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+					
 
-						        </li>
 
-								<li data-transition="boxfade" data-slotamount="20" class="active-revslide current-sr-slide-visible" style="width: 100%; height: 100%; overflow: hidden; visibility: inherit; opacity: 1; z-index: 20;">
-						            <div class="slotholder" style="width:100%;height:100%;" data-duration="undefined" data-zoomstart="undefined" data-zoomend="undefined" data-rotationstart="undefined" data-rotationend="undefined" data-ease="undefined" data-bgpositionend="undefined" data-bgposition="undefined" data-kenburns="undefined" data-easeme="undefined" data-bgfit="undefined" data-bgfitend="undefined" data-owidth="undefined" data-oheight="undefined">
-													<div class="tp-bgimg defaultimg" data-lazyload="undefined" data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat" data-lazydone="undefined" src="source/assets/dest/images/thumbs/1.jpg" data-src="source/assets/dest/images/thumbs/1.jpg" style="background-color: rgba(0, 0, 0, 0); background-repeat: no-repeat; background-image: url('assets/dest/images/thumbs/1.jpg'); background-size: cover; background-position: center center; width: 100%; height: 100%; opacity: 1; visibility: inherit;">
-												</div>
-											</div>
-
-						        </li> --}}
-								</ul>
-							</div>
-						</div>
-
-						<div class="tp-bannertimer"></div>
-					</div>
-				</div>
-				<!--slider-->
-	</div>
-	<div class="container">
-		<div id="content" class="space-top-none">
-			<div class="main-content">
-				<div class="space60">&nbsp;</div>
-				<div class="row">
-					<div class="col-sm-12">
-						<div class="beta-products-list">
-							<h4>Sản phẩm mới</h4>
-							<div class="beta-products-details">
-								<p class="pull-left"><a href="{{  route('spmoi') }} ">Xem thêm</a></p>
-								<div class="clearfix"></div>
-							</div>
-
-							<div class="row">
-								@foreach($new_product as $item)
-								<div class="col-sm-3" style="margin-top: 20px">
-
-									<div class="single-item">
-										@if($item->promotion_price != null)
-										<div class="ribbon-wrapper"><div class="ribbon sale">Sale</div></div>
-										@endif
-										<div class="single-item-header">
-											<a href="{{ route('chitietsp',$item->id) }}"><img style="height: 250px" src="source/image/product/{{ $item->image }}" alt=""></a>
-										</div>
-										<div class="single-item-body">
-											<p class="single-item-title">{{ $item->name }}</p>
-											<p class="single-item-price">
-											@if($item->promotion_price == 0)
-												<span>{{ number_format($item->unit_price) }}đ/{{ $item->unit }}</span>
-											
-											
-											@else
-												<span class="flash-del">{{ number_format($item->unit_price) }}đ/{{ $item->unit }}</span>
-												<span class="flash-sale">{{ number_format($item->promotion_price) }}đ/{{ $item->unit }}</span>
-												
-											@endif
-											</p>
-										</div>
-										<div class="single-item-caption" style="margin-top: 10px">
-											
-											
-											<a class="add-to-cart pull-left" href="{{route('themgiohang',$item->id)}}"><i class="fa fa-shopping-cart"></i></a>
-											<a class="beta-btn primary" href="{{ route('chitietsp',$item->id) }}">Details <i class="fa fa-chevron-right"></i></a>
-											
-										</div>
-									</div>
-								</div>
-								@endforeach
-							</div>
-						</div> <!-- .beta-products-list -->
-						
-   
-						<div class="space50">&nbsp;</div>
-
-						<div class="beta-products-list">
-							<h4>Sản phẩm khuyến mãi</h4>
-							<div class="beta-products-details">
-
-								<p class="pull-left"><a href="{{ route('spkhuyenmai') }}">Xem thêm</a></p>
-								<div class="clearfix"></div>
-							</div>
+					
 							
+					
+					<!--slider-->
+				
+				<div class="container">
+					<div id="content" class="space-top-none">
+						<div class="main-content">
+							<div class="space60">&nbsp;</div>
 							<div class="row">
-								@foreach($sanpham_khuyenmai as $item)
-								<div class="col-sm-3"  style="margin-top: 20px">
-									<div class="single-item">
-										<div class="ribbon-wrapper"><div class="ribbon sale">Sale</div></div>
-										<div class="single-item-header">
-											<a href="{{ route('chitietsp',$item->id) }}"><img style="height: 250px" src="source/image/product/{{ $item->image }}" alt=""></a>
-										</div>
-										<div class="single-item-body">
-											<p class="single-item-title">{{ $item->name }}</p>
-											<p class="single-item-price">
-												<span class="flash-del">{{ number_format($item->unit_price) }}đ/{{ $item->unit }}</span>
-												<span class="flash-sale">{{ number_format($item->promotion_price) }}đ/{{ $item->unit }}</span>
-											</p>
-										</div>
-										<div class="single-item-caption" style="margin-top: 10px">
-
-											<a class="add-to-cart pull-left" href="{{route('themgiohang',$item->id)}}"><i class="fa fa-shopping-cart"></i></a>
-											<a class="beta-btn primary" href="{{ route('chitietsp',$item->id) }}">Details <i class="fa fa-chevron-right"></i></a>
+								<div class="col-sm-12">
+									<div class="beta-products-list">
+										<h4>Sản phẩm mới</h4>
+										<div class="beta-products-details">
+											<p class="pull-left"><a href="{{  route('spmoi') }} ">Xem thêm</a></p>
 											<div class="clearfix"></div>
 										</div>
-									</div>
+
+										<div class="row">
+											@foreach($new_product as $item)
+											<div class="col-sm-3" style="margin-top: 20px">
+
+												<div class="single-item">
+													@if($item->promotion_price != null)
+													<div class="ribbon-wrapper"><div class="ribbon sale">Sale</div></div>
+													@endif
+													<div class="single-item-header">
+														<a href="{{ route('chitietsp',$item->id) }}"><img style="height: 250px" src="source/image/product/{{ $item->image }}" alt=""></a>
+													</div>
+													<div class="single-item-body">
+														<p class="single-item-title">{{ $item->name }}</p>
+														<p class="single-item-price">
+															@if($item->promotion_price == 0)
+															<span>{{ number_format($item->unit_price) }}đ/{{ $item->unit }}</span>
+
+
+															@else
+															<span class="flash-del">{{ number_format($item->unit_price) }}đ/{{ $item->unit }}</span>
+															<span class="flash-sale">{{ number_format($item->promotion_price) }}đ/{{ $item->unit }}</span>
+
+															@endif
+														</p>
+													</div>
+													<div class="single-item-caption" style="margin-top: 10px">
+
+
+														<a class="add-to-cart pull-left" href="{{route('themgiohang',$item->id)}}"><i class="fa fa-shopping-cart"></i></a>
+														<a class="beta-btn primary" href="{{ route('chitietsp',$item->id) }}">Details <i class="fa fa-chevron-right"></i></a>
+
+													</div>
+												</div>
+											</div>
+											@endforeach
+										</div>
+									</div> <!-- .beta-products-list -->
+
+
+									<div class="space50">&nbsp;</div>
+
+									<div class="beta-products-list">
+										<h4>Sản phẩm khuyến mãi</h4>
+										<div class="beta-products-details">
+
+											<p class="pull-left"><a href="{{ route('spkhuyenmai') }}">Xem thêm</a></p>
+											<div class="clearfix"></div>
+										</div>
+
+										<div class="row">
+											@foreach($sanpham_khuyenmai as $item)
+											<div class="col-sm-3"  style="margin-top: 20px">
+												<div class="single-item">
+													<div class="ribbon-wrapper"><div class="ribbon sale">Sale</div></div>
+													<div class="single-item-header">
+														<a href="{{ route('chitietsp',$item->id) }}"><img style="height: 250px" src="source/image/product/{{ $item->image }}" alt=""></a>
+													</div>
+													<div class="single-item-body">
+														<p class="single-item-title">{{ $item->name }}</p>
+														<p class="single-item-price">
+															<span class="flash-del">{{ number_format($item->unit_price) }}đ/{{ $item->unit }}</span>
+															<span class="flash-sale">{{ number_format($item->promotion_price) }}đ/{{ $item->unit }}</span>
+														</p>
+													</div>
+													<div class="single-item-caption" style="margin-top: 10px">
+
+														<a class="add-to-cart pull-left" href="{{route('themgiohang',$item->id)}}"><i class="fa fa-shopping-cart"></i></a>
+														<a class="beta-btn primary" href="{{ route('chitietsp',$item->id) }}">Details <i class="fa fa-chevron-right"></i></a>
+														<div class="clearfix"></div>
+													</div>
+												</div>
+											</div>
+
+											@endforeach
+
+										</div>
+									</div> <!-- .beta-products-list -->
 								</div>
-
-								@endforeach
-								
-							</div>
-						</div> <!-- .beta-products-list -->
-					</div>
-				</div> <!-- end section with sidebar and main content -->
+							</div> <!-- end section with sidebar and main content -->
 
 
-			</div> <!-- .main-content -->
-		</div> <!-- #content -->
-	</div> <!-- .container -->
-@endsection
+						</div> <!-- .main-content -->
+					</div> <!-- #content -->
+				</div> <!-- .container -->
+				@endsection
 
-<script>
-	$(document).on('click','.pagination a',function(e){
-		e.preventDefault();
-		var page = $(this).attr('href')split('page=')[1];
-		getSaleProduct(page);
-	});
-	function getSaleProduct(page){
-			$.ajax({
-				url: '/index?page='+page
-			}).done(function(data){
-				$('.row').html(data);
-			});
-	}
+				<script>
+					$(document).on('click','.pagination a',function(e){
+						e.preventDefault();
+						var page = $(this).attr('href')split('page=')[1];
+						getSaleProduct(page);
+					});
+					function getSaleProduct(page){
+						$.ajax({
+							url: '/index?page='+page
+						}).done(function(data){
+							$('.row').html(data);
+						});
+					}
 
-</script>
+				</script>
